c1facfb84ddd1c2a0849534fdef21dc1d7eb8d54
sadamyan <adamyanssergey@gmail.com>
2024-02-19 13:07:09 +0400

    Models

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import MenuItem
+from .models import Booking
+
+
+admin.site.register(Booking)
+admin.site.register(MenuItem)
